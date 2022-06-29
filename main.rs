cde0a9b19760205c86b0a799d4fab211c234c95a
Sumeet Agarwal <sumeet.a@gmail.com>
2022-06-28 20:46:26 -0700

    add close

--- main.rs
+++ main.rs
@@ -10,7 +10,7 @@ const AF_INET: i32 = 2;
 
 type SocklenT = u32;
 
-const PAYLOAD: &'static str = r#"<marquee>Hello, Web!</marquee>"#;
+const FILE_NAME: &'static str = "index.htm";
 
 fn write_num(fd: i32, mut num: usize) {
     if num == 0 {
@@ -33,6 +33,206 @@ fn write_num(fd: i32, mut num: usize) {
     write(fd, &buffer[i..BUFFER_SIZE]);
 }
 
+fn write_http_resp_from_filename(out_fd: i32, filename: &str) {
+    let fd = open(filename);
+    let stats = fstat(fd);
+    write_http_resp_header(out_fd, stats.st_size as _);
+
+    let mut buf: [u8; 32] = [
+        0u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0,
+    ];
+    loop {
+        let bytes_read = read(fd, &mut buf);
+        if bytes_read > 0 {
+            write(out_fd, &buf[..bytes_read as _]);
+        } else {
+            break;
+        }
+    }
+    close(fd);
+}
+
+fn read(fd: i32, buf: &mut [u8]) -> u32 {
+    const SYSCALL_READ: u64 = 0;
+    let res: i64;
+    unsafe {
+        //  ssize_t read(int fd, void *buf, size_t count);
+        asm!(
+        "syscall",
+        inout("rax") SYSCALL_READ => res,
+        // argument #1
+        in("rdi") fd,
+        // argument #2
+        in("rsi") buf.as_ptr(),
+        // argument #2
+        in("rdx") buf.len(),
+        )
+    }
+
+    if res < 0 {
+        panic!("read failed");
+    }
+    res as u32
+}
+
+fn close(fd: i32) {
+    const SYSCALL_CLOSE: u64 = 3;
+    let res: i64;
+    unsafe {
+        // int close(int fd);
+        asm!(
+        "syscall",
+        inout("rax") SYSCALL_CLOSE => res,
+        // argument #1
+        in("rdi") fd,
+        )
+    }
+
+    if res < 0 {
+        panic!("close failed");
+    }
+}
+
+fn open(path: &str) -> i32 {
+    // converting a rust string into a c string
+    const BUFFER_SIZE: usize = 1024;
+    #[allow(deprecated)]
+    // TODO: can't get uninitialized() to work because it says it wants memcpy. how to not have to
+    // paste this giant array literal?
+    // let mut buffer: [u8; BUFFER_SIZE] = unsafe { core::mem::uninitialized() };
+    let mut buffer: [u8; BUFFER_SIZE] = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0,
+    ];
+    let path_bytes = path.as_bytes();
+    if path_bytes.len() + 1 > BUFFER_SIZE {
+        panic!("path is too long");
+    }
+    for i in 0..path.len() {
+        buffer[i] = path_bytes[i];
+    }
+    buffer[path.len()] = 0;
+
+    let fd: i32;
+    let filepath_cstr = &buffer as *const u8;
+
+    const SYSCALL_OPEN: i32 = 2;
+    const O_RDONLY: i32 = 0;
+
+    unsafe {
+        // int open(char *file, int omode)
+        asm!(
+        "syscall",
+        inout("rax") SYSCALL_OPEN => fd,
+        // argument #1
+        in("rdi") filepath_cstr,
+        // argument #2
+        in("rsi") O_RDONLY,
+        )
+    }
+
+    if fd < 0 {
+        panic!("open failed");
+    }
+    fd
+}
+
+// copilot wrote this struct, we can grab the size, not sure if it's even the correct size
+// so maybe fstat is like writing into garbage memory because the struct isn't big enough
+#[repr(C)]
+struct Stat {
+    st_dev: u64,
+    st_ino: u64,
+    st_mode: u32,
+    st_nlink: u64,
+    st_uid: u32,
+    st_gid: u32,
+    st_rdev: u64,
+    st_size: i64,
+    st_blksize: u64,
+    st_blocks: u64,
+    st_atime: u64,
+    st_atime_nsec: u64,
+    st_mtime: u64,
+    st_mtime_nsec: u64,
+    st_ctime: u64,
+    st_ctime_nsec: u64,
+}
+
+// fstat => 5
+fn fstat(fd: i32) -> Stat {
+    const SYSCALL_FSTAT: i32 = 5;
+    let mut stat: Stat = Stat {
+        st_dev: 0,
+        st_ino: 0,
+        st_mode: 0,
+        st_nlink: 0,
+        st_uid: 0,
+        st_gid: 0,
+        st_rdev: 0,
+        st_size: 0,
+        st_blksize: 0,
+        st_blocks: 0,
+        st_atime: 0,
+        st_atime_nsec: 0,
+        st_mtime: 0,
+        st_mtime_nsec: 0,
+        st_ctime: 0,
+        st_ctime_nsec: 0,
+    };
+    let mut res: i32;
+    unsafe {
+        // int fstat(int fd, struct stat *buf)
+        asm!(
+        "syscall",
+        inout("rax") SYSCALL_FSTAT => res,
+        // argument #1
+        in("rdi") fd,
+        // argument #2
+        in("rsi") &mut stat as *mut Stat,
+        )
+    }
+
+    if res < 0 {
+        panic!("stat failed");
+    }
+    stat
+}
+
 fn write_http_resp_header(fd: i32, len: usize) {
     write(
         fd,
@@ -53,8 +253,7 @@ fn _start() {
     listen(fd);
     loop {
         let conn_fd = accept(fd);
-        write_http_resp_header(conn_fd, PAYLOAD.len());
-        write(conn_fd, PAYLOAD.as_bytes());
+        write_http_resp_from_filename(conn_fd, FILE_NAME);
         print("Send response to client who connected\n");
     }
 }
